5838e6d5aef6f3fa09086c1a4d571fd7a04d1b9a
Stacey McKnight <mcknight.stm@gmail.com>
2021-11-11 20:42:08 -0600

    style micropost elements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,6 +5,6 @@ module UsersHelper
     size         = options[:size]
     gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar rounded-full bg-green-50 h-18 w-18 flex items-center justify-center") 
   end
 end
